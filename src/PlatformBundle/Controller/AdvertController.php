94c4ffc2ed0073a99224f18b9a3fa02bfcaf6a83
Andy Bar <admin@server.com>
2017-11-19 23:14:38 +0100

    Entities mofified

--- src/PlatformBundle/Controller/AdvertController.php
+++ src/PlatformBundle/Controller/AdvertController.php
@@ -3,6 +3,11 @@
 namespace PlatformBundle\Controller;
 
 use PlatformBundle\Helpers\Test;
+use PlatformBundle\Entity\Advert;
+use PlatformBundle\Entity\Image;
+use PlatformBundle\Entity\Application;
+use PlatformBundle\Entity\Skill;
+use PlatformBundle\Entity\AdvertSkill;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -46,29 +51,138 @@ class AdvertController extends Controller
 
     public function viewAction($id)
     {
-        $advert = array(
-            'title'   => 'Recherche développpeur Symfony2',
-            'id'      => $id,
-            'author'  => 'Alexandre',
-            'content' => 'Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…',
-            'date'    => new \Datetime()
-        );
+        $em = $this->getDoctrine()->getManager();
+
+        // On récupère le repository
+        //$repository = $em->getRepository('PlatformBundle:Advert');
+        $repository = $em->getRepository('PlatformBundle\Entity\Advert');
+        //var_dump($repository);
+
+        // On récupère l'entité correspondante à l'id $id
+        $advert = $repository->find($id);
+
+        // $advert est donc une instance de OC\PlatformBundle\Entity\Advert
+        // ou null si l'id $id  n'existe pas, d'où ce if :
+        if (null === $advert) {
+            throw new NotFoundHttpException("L'annonce d'id ".$id." n'existe pas.");
+        }
+
+        //$res = $repository->getAdvertWithCategories(array('Graphisme'));
+
+        $res = $repository->myFindAll();
+        //var_dump($res);
+        // On récupère la liste des candidatures de cette annonce
+        /*$listApplications = $em
+            ->getRepository('PlatformBundle:Application')
+            ->findBy(array('advert' => $advert))
+        ;*/
+
+        $listApplications = $advert->getApplications();
+        //var_dump($listApplications);
+
+        // On récupère maintenant la liste des AdvertSkill
+
+        $listAdvertSkills = $em
+            ->getRepository('PlatformBundle:AdvertSkill')
+            ->findBy(array('advert' => $advert))
+        ;
 
         return $this->render('PlatformBundle:Advert:view.html.twig', array(
-            'advert' => $advert
+            'advert' => $advert,
+            'listApplications' => $listApplications,
+            'listAdvertSkills' => $listAdvertSkills,
+            'result' => $res
         ));
     }
 
     public function addAction(Request $request)
     {
-        $antispam = $this->container->get('platform.antispam');
+        /*$antispam = $this->container->get('platform.antispam');
         $text = '...';
         //var_dump($antispam);
         if ($antispam->isSpam($text)) {
             throw new \Exception('Votre message a été détecté comme spam !');
+        }*/
+        $em = $this->getDoctrine()->getManager();
+
+        $advert = new Advert();
+        $advert->setTitle('PHP developer');
+        $advert->setAuthor('Paul');
+        $advert->setContent('Recrutons un bon back-end developer');
+        $advert->setSlug('front_end_developer');
+
+        /*$image = new Image();
+        $image->setUrl('http://sdz-upload.s3.amazonaws.com/prod/upload/job-de-reve.jpg');
+        $image->setAlt('Job de rêve');
+
+        $advert->setImage($image);*/
+
+        // Création d'une première candidature
+        $application1 = new Application();
+        $application1->setAuthor('Maurice');
+        $application1->setContent("Bonjour.");
+
+        $application1->setAdvert($advert);
+
+
+        // Création d'une deuxième candidature par exemple
+        /*$application2 = new Application();
+        $application2->setAuthor('Pierre');
+        $application2->setContent("Je suis très motivé.");
+
+        // On lie les candidatures à l'annonce
+        $application1->setAdvert($advert);
+        $application2->setAdvert($advert);
+
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($advert);
+
+        // Étape 1 bis : pour cette relation pas de cascade lorsqu'on persiste Advert, car la relation est
+        // définie dans l'entité Application et non Advert. On doit donc tout persister à la main ici.
+        $em->persist($application1);
+        $em->persist($application2);
+
+        $em->flush();
+*/
+        $em->persist($application1);
+        // On lie la candidature à l'annonce
+        $advert->addApplication($application1);
+
+
+        // On récupère toutes les compétences possibles
+        $listSkills = $em->getRepository('PlatformBundle:Skill')->findAll();
+
+        // Pour chaque compétence
+        foreach ($listSkills as $skill) {
+            // On crée une nouvelle « relation entre 1 annonce et 1 compétence »
+            $advertSkill = new AdvertSkill();
+
+            // On la lie à l'annonce, qui est ici toujours la même
+            $advertSkill->setAdvert($advert);
+            // On la lie à la compétence, qui change ici dans la boucle foreach
+            $advertSkill->setSkill($skill);
+
+            // Arbitrairement, on dit que chaque compétence est requise au niveau 'Expert'
+            $advertSkill->setLevel('Avisé');
+
+            // Et bien sûr, on persiste cette entité de relation, propriétaire des deux autres relations
+            $em->persist($advertSkill);
         }
 
-        $form = null;
+        // Doctrine ne connait pas encore l'entité $advert. Si vous n'avez pas définit la relation AdvertSkill
+        // avec un cascade persist (ce qui est le cas si vous avez utilisé mon code), alors on doit persister $advert
+        $em->persist($advert);
+
+        // On déclenche l'enregistrement
+        $em->flush();
+
+        $session = $request->getSession();
+        $session->getFlashBag()->add('info', 'Annonce bien enregistrée');
+
+        return $this->redirectToRoute('platform_view', array('id' => $advert->getId()));
+
+        /*$form = null;
         if ($request->isMethod('POST')) {
             $session = $request->getSession();
             $session->getFlashBag()->add('info', 'Annonce bien enregistrée');
@@ -76,26 +190,41 @@ class AdvertController extends Controller
             //return new JsonResponse(array('id' => 5, 'param' => $param, 'headers' => json_encode($headers)));
 
             return $this->redirectToRoute('platform_view', array('id' => 6));
-        }
-        return $this->render('PlatformBundle:Advert:add.html.twig', array('form' => $form));
+        }*/
+        //return $this->render('PlatformBundle:Advert:add.html.twig', array('form' => $form));
     }
 
 
     public function editAction($id, Request $request)
     {
+        $em = $this->getDoctrine()->getManager();
+        $advert = $em->getRepository('PlatformBundle:Advert')->find($id);
+
+        if (null === $advert) {
+            throw new NotFoundHttpException("L'annonce d'id ".$id." n'existe pas.");
+        }
+
+        $arCategories = $em->getRepository('PlatformBundle:Category')->findAll();
+
+        foreach ($arCategories as $category) {
+            $advert->addCategory($category);
+        }
+        $em->flush();
+
+
         if ($request->isMethod('POST')) {
             $request->getSession()->getFlashBag()->add('notice', 'Annonce bien modifiée.');
 
             return $this->redirectToRoute('platform_view', array('id' => 5));
         }
 
-        $advert = array(
+        /*$advert = array(
             'title'   => 'Recherche développpeur Symfony2',
             'id'      => $id,
             'author'  => 'Alexandre',
             'content' => 'Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…',
             'date'    => new \Datetime()
-        );
+        );*/
 
         return $this->render('PlatformBundle:Advert:edit.html.twig', array(
             'advert' => $advert
